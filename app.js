7553c97f30d2e9e5d0011451e286d73276c3331a
Charles Welsh <charleswelsh@gmail.com>
2020-06-01 16:02:48 -0500

    lab complete

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ function setName() {
     var name = 'Covalence';
     console.log(name);
 }
+let avgResult= findAvg(3,6);
+console.log("this is the average of the two provided numbers ==> " + avgResult);
+
+function findAvg(a,b){
+let answer = (a + b) / 2;
+return answer;
+}
+
+let fruits = [
+    "apples",
+    "oranges",
+    "bananas",
+    "lemons",
+];
+
+function findFruit() {
+    let favFruit = fruits[2];
+    let worstFruit = fruits[3];
+    console.log(fruits[0]);
+    
+    function printfavFruit() {
+        console.log(favFruit);
+    };
+
+    printfavFruit();
+    console.log(worstFruit);
+};
+
+findFruit();
